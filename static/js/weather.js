b65c6b64cac69b99c969a859643bc1a974abc5ed
Wilku <a.szumacher@boosthigh.com>
2023-04-04 20:00:35 +0200

    add weather and styling

--- static/js/weather.js
+++ static/js/weather.js
@@ -2,7 +2,6 @@ let counter = 0
 
 function getWeather() {
     $.get("/weather", function(data) {
-        console.warn(data);
         $("#weather").text(`${data.current_weather.temperature}${data.hourly_units.temperature_2m}`);
     });
 }

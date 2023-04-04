9665deae987f88015267d0d7ed63df193aab5157
Wilku <a.szumacher@boosthigh.com>
2023-04-04 19:47:56 +0200

    add weather and styling

---
+++ static/js/weather.js
@@ -0,0 +1,24 @@
+let counter = 0
+
+function getWeather() {
+    $.get("/weather", function(data) {
+        console.warn(data);
+        $("#weather").text(`${data.current_weather.temperature}${data.hourly_units.temperature_2m}`);
+    });
+}
+setInterval(getWeather, 2000);
+
+
+/*
+ "current_weather": {
+    "time": "2022-01-01T15:00"
+    "temperature": 2.4, "weathercode": 3,
+    "windspeed": 11.9, "winddirection": 95.0,
+  },
+  "hourly": {
+    "time": ["2022-07-01T00:00","2022-07-01T01:00", ...]
+    "windspeed_10m": [3.16,3.02,3.3,3.14,3.2,2.95, ...],
+    "temperature_2m": [13.7,13.3,12.8,12.3,11.8, ...],
+    "relativehumidity_2m": [82,83,86,85,88,88,84,76, ...],
+  }
+  */

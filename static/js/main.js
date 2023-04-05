93ff6e7cc2588fc53bddab468416dba17481e22e
Wilku <a.szumacher@boosthigh.com>
2023-04-05 08:37:36 +0200

    remove request to be by date

--- static/js/main.js
+++ static/js/main.js
@@ -1,27 +1,36 @@
 function updateTime() {
-    $.get("/time", function (data) {
-        $("#time").text(data[0]);
-        $("#day").text(`${setDay(data[1])} ${data[2]}`);
-    });
+    // $.get("/time", function (data) {
+    //     $("#time").text(data[0]);
+    //     $("#day").text(`${setDay(data[1])} ${data[2]}`);
+    // });
+    let objectDate = new Date();
+    let day = objectDate.getDate();
+    let dayNumber = objectDate.getDay();
+    let month = objectDate.getMonth() +1;
+    let year = objectDate.getFullYear();
+    let date = `${day}.${month}.${year}`;
+
+    $("#time").text(`${objectDate.getHours()}:${objectDate.getMinutes()}`);
+    $("#day").text(`${setDay(dayNumber)} ${date}`);
 }
 
 setInterval(updateTime, 1000);
 
 function setDay(day) {
     switch (Number(day)) {
-        case 0:
-            return 'Poniedziałek'
         case 1:
-            return 'Wtorek'
+            return 'Poniedziałek'
         case 2:
-            return 'Środa'
+            return 'Wtorek'
         case 3:
-            return 'Czwartek'
+            return 'Środa'
         case 4:
-            return 'Piątek'
+            return 'Czwartek'
         case 5:
-            return 'Sobota'
+            return 'Piątek'
         case 6:
+            return 'Sobota'
+        case 0:
             return 'Niedziela'
 
     }

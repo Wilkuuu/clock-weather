c6195bb430b1dd7f5b893949705d2df147695b85
Wilku <a.szumacher@boosthigh.com>
2023-04-06 07:42:59 +0200

    fix env

--- time.py
+++ time.py
@@ -1,12 +1,17 @@
+import dotenv
 from flask import Flask, render_template
+from dotenv import load_dotenv
 from datetime import datetime
+import os
 import requests
 
+load_dotenv()
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
+    print(os.getenv('API_KEY'))
     return render_template("index.html")
 
 
@@ -15,9 +20,7 @@ def get_weather():
 #     response = requests.get(
 #         "https://api.open-meteo.com/v1/forecast?latitude=53.22&longitude=18.14&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m")
     response = requests.get(
-        "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/aleksandrowo%2Cdobrcz?unitGroup=metric&key=CA9MXP5FRAKJLRMPQ8L6G8T2B&contentType=json")
+        "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/aleksandrowo%2Cdobrcz?unitGroup=metric&key=" + os.getenv('API_KEY') + "&contentType=json")
     return response.json()
-# CA9MXP5FRAKJLRMPQ8L6G8T2B
-# L47MMDSK79SLHF4F38VSZN2G7
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
